a87479e7aadf40606ea4b4710486fb9d86a17325
Ivan Christian Jay Funcion <icjfuncion@gmail.com>
2023-01-01 18:21:12 +0800

    done with assembler

--- src/main/java/com/ivanfuncion/rest_service/assembler/EmployeeModelAssembler.java
+++ src/main/java/com/ivanfuncion/rest_service/assembler/EmployeeModelAssembler.java
@@ -4,9 +4,11 @@ import com.ivanfuncion.rest_service.controller.EmployeeController;
 import com.ivanfuncion.rest_service.model.Employee;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
+@Component
 public class EmployeeModelAssembler implements RepresentationModelAssembler<Employee, EntityModel<Employee>> {
 
 
